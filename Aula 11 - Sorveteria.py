b3287f7dd10879f6944626a0b570e73f8b199b42
mikef21 <148161781+mikef21@users.noreply.github.com>
2023-11-13 22:46:20 -0300

    Add files via upload

---
+++ Aula 11 - Sorveteria.py
@@ -0,0 +1,9 @@
+# Importação das Bibliotecas
+
+import pandas as pd # Importar dados externos
+
+import numpy as np # Trabalhar com algebra linear
+
+import matplotlib.pyplot as plt # Vizualiação de dados
+
+from sklearn.linear_model import LinearRegression # Machine Learning, carregando o algoritmo de regressão
